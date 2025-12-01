a006f24d008bb77bc8b22919661d62e529756b30
AbrorCPP <abrorboltayev4843@gmail.com>
2025-12-01 18:21:50 +0500

    123

--- Taxistadt 1.0/park.py
+++ Taxistadt 1.0/park.py
@@ -10,7 +10,7 @@ class User:
 
 
 class Car:
-    def __init__(self, model, brand, year, seria, ):
+    def __init__(self, model, brand, year, seria ):
         self.model  = model
         self.brand  = brand
         self.year   = year
@@ -19,11 +19,17 @@ class Car:
 
 class Order:
     def __init__(self,user_id,car_id,date_start,date_end):
-        self.user_id    =user_id
-        self.car_id     =car_id
-        self.date_start =date_start
-        self.date_end   =date_end
+        self.user_id    = user_id
+        self.car_id     = car_id
+        self.date_start = date_start
+        self.date_end   = date_end
         self.is_active  = True
+
+class Massage:
+    def __init__(self,user_id,car_id):
+        self.user_id = user_id
+        self.car_id = car_id
+        self.Let = False
 
 class Park:
     def __init__(self,title):
@@ -31,6 +37,36 @@ class Park:
         self.users   =[]
         self.cars    =[]
         self.orders  =[]
+        self.massages = []
+
+    def show_massages(self):
+        i =0
+        for ms in self.massages:
+            i+=1
+            print(f"{i}. {self.users[ms.user_id].username} wants to take {self.cars[ms.car_id].title}.")
+        a = int(input("Select one: "))
+        i = 0
+        for ms in  self.massages:
+            i+=1
+            if  a == i:
+                t = input("Ruxsat berasizmi\n(1) Ha (2) Yo'q \n--->")
+                if t == "1":
+                    user_id = ms.user_id
+                    car_id = ms.car_id
+                    date_start = input("Enter start date: ")
+                    date_end = input("Enter end date: ")
+                    order = Order(user_id, car_id, date_start, date_end)
+                    self.orders.append(order)
+                    count = 0
+                    for car in self.cars:
+                        count += 1
+                        if count == car_id:
+                            car.is_active = False
+                    count = 0
+                    for user in self.users:
+                        count += 1
+                        if count == user_id:
+                            user.is_active = False
 
     def add_user(self):
         username = input("Enter username: ")
@@ -54,6 +90,26 @@ class Park:
         for car in self.cars:
             count += 1
             print(f"{count}.{car.model} - {car.brand} - {car.year} - {car.seria}")
+
+    def book_cars(self,username):
+        usern_id = 0
+        count = 0
+        for user in self.users:
+            count+=1
+            if user.username == username:
+                usern_id = count
+        count = 0
+        for car in self.cars:
+            if car.is_active:
+                count += 1
+                print(f"{count}.{car.model} - {car.brand} - {car.year} - {car.seria} ---")
+        a = int(input("Enter an id: ")) - 1
+        count = 0
+        for car in self.cars:
+            count+=1
+            if count == a:
+                s = Massage(usern_id,a)
+                self.massages.append(s)
 
     def show_all_users(self):
         count = 0
@@ -193,17 +249,19 @@ def park_manager(p:Park):
         elif a == 1:
             while True:
                 print("User menu")
-                b1 = input(" 1.Show cars \n 2.Change settings \n 4.Exit\n --->")
+                b1 = input(" 1.Show cars \n 2.Change settings \n 3.Book a car\n 4.Exit\n --->")
                 if b1 == "1":
                     p.show_cars_not_users()
                 elif b1 == "2":
                     p.edit_user(us,pa)
+                elif b1 == "3":
+                    p.book_cars(us)
                 else:
                     break
         elif a == 0:
             while True:
                 print("Admin menu")
-                b2 = input(" 1.Add menu\n 2.Show menu\n 3.Order menu\n 4.Exit\n --->")
+                b2 = input(" 1.Add menu\n 2.Show menu\n 3.Order menu\n 4.Show massages\n 5.Exit --->")
                 if b2 == "1":
                     while True:
                         b3 = input(" 1.Add user\n 2.Add car\n 3.Exit\n --->")
@@ -237,6 +295,8 @@ def park_manager(p:Park):
                             p.ignore_order()
                         else:
                             break
+                elif b2 == "4":
+                    p.show_massages()
                 else:
                     break
 
